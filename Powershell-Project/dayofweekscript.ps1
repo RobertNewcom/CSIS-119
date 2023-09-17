1a85ba23e421e847e7b8f7424efbf5091b1d43ec
RobertNewcom <112905696+RobertNewcom@users.noreply.github.com>
2023-09-17 18:18:33 -0500

    Update dayofweekscript.ps1

--- Powershell-Project/dayofweekscript.ps1
+++ Powershell-Project/dayofweekscript.ps1
@@ -1,4 +1,20 @@
 #pwrsh script
 
-  $day = "Wednesday"
+$day = Read-Host "Which day of the week is sciprting class for Robert?: "
 
+ $useranswer = switch ( $day )
+  
+{
+        {$useranswer { 'Monday' Write-Output  "NOOOOOOOOOOOOOOO"} 
+
+
+
+
+
+
+
+
+
+
+}
+ $useranswer
