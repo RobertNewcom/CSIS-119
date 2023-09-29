05f45231b0491789f4dc26cdf9b0df346df930e9
RobertNewcom <112905696+RobertNewcom@users.noreply.github.com>
2023-09-28 20:04:27 -0500

    Update isroberton.ps1
    Format fix

--- Powershell-Project/isroberton.ps1
+++ Powershell-Project/isroberton.ps1
@@ -1,12 +1,16 @@
 # Powershell meets requirements for if statement and for loop
 # CSIS-119 Robert Newcom 9/25/23
+
 # Get current user from windows security and apply to $user
 # Code ran on personal windows computer and is referencing my personal user id
 $user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
 # Check for the user a maximum of 10 times
 $num_checks = 10
+
 # Time delay before the next check 
 $interval = 10
+
 # Loop with for and if statements
 for ($i = 1; $i -le $num_checks; $i++) #-le is less
 {
